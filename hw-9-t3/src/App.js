b4b5f357032868b2cc267e761a828d086c20aabd
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-22 22:01:20 +0300

    fifth

--- hw-9-t3/src/App.js
+++ hw-9-t3/src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import { useHistory } from "react-router";
 import "./App.css";
 
 function App() {
   const history = useHistory();
-  const richDadTiltle = "Rich Dad Poor Dad"
-  const theRichest = "The Richest Man In Babylon"
+  const richDadTiltle = "Rich Dad Poor Dad";
+  const theRichest = "The Richest Man In Babylon";
 
   const handleClick = (name) => {
     history.push(`/about/${name}`);
@@ -13,7 +14,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={() => handleClick(richDadTiltle)}>{richDadTiltle}</button>
+      <button onClick={() => handleClick(richDadTiltle)}>
+        {richDadTiltle}
+      </button>
       <button onClick={() => handleClick(theRichest)}>{theRichest}</button>
     </div>
   );
